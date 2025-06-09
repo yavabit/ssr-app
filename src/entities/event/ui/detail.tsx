ab1f46ace015c489808f2b15fdb35e899698c494
Anton Zhilenkov <a.zhilenkov@globexit.ru>
2025-06-09 23:27:48 +0300

    feat: leave & update events added

--- src/entities/event/ui/detail.tsx
+++ src/entities/event/ui/detail.tsx
@@ -1,4 +1,7 @@
 import { RouterOutput } from "@/shared/api";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 type EventDetailProps = NonNullable<RouterOutput["event"]["findUnique"]>;
 
@@ -6,14 +9,24 @@ export const EventDetail = ({
   title,
   description,
   date,
+  authorId,
   participations,
 }: EventDetailProps) => {
+
+  const {data: session} = useSession();
+  const router = useRouter();
+
   return (
     <div>
-      <div className="px-4 sm:px-0">
+      <div className="px-4 sm:px-0 flex items-center justify-between">
         <h3 className="text-base font-semibold leading-7 text-gray-900">
           Информация о событии
         </h3>
+        {session && session.user.id === authorId && (
+          <Link href={`/events/update/${router.query.id}`} className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 transition-colors">
+            Редактировать событие
+          </Link>
+        )}
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
 import { EventCard } from "@/entities/event";
 import { JoinEventButton } from "@/features/join-event";
+import { LeaveEventButton } from "@/features/leave-event";
 import { trpc } from "@/shared/api";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data, refetch } = trpc.event.findMany.useQuery();
+	const { data, refetch } = trpc.event.findMany.useQuery();
+	const { data: session } = useSession();
 
-  return (
-    <ul>
-      {data?.map((event) => (
-        <li key={event.id} className="mb-6">
-          <EventCard
-            {...event}
-            action={
-              !event.isJoined && (
-                <JoinEventButton eventId={event.id} onSuccess={refetch} />
-              )
-            }
-          />
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<ul>
+			{data?.map((event) => (
+				<li key={event.id} className="mb-6">
+					<EventCard
+						{...event}
+						action={
+							session && (
+								<>
+									{!event.isJoined ? (
+										<JoinEventButton eventId={event.id} onSuccess={refetch} />
+									) : (
+										<LeaveEventButton eventId={event.id} onSuccess={refetch} />
+									)}
+								</>
+							)
+						}
+					/>
+				</li>
+			))}
+		</ul>
+	);
 }
